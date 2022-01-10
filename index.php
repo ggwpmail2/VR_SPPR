d03bc2a86963765c96d5d820e1acea856fd1f94c
ggwpmail2 <ggwpmail2@gmail.com>
2022-01-10 13:13:37 +0800

    rus1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>Snippet - BBBootstrap</title>
+    <title>Система поддержки принятия решения выбора оборудования виртуальной реальности</title>
     <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
     <script type='text/javascript' src='script.js'></script>
     <link rel="stylesheet" href="style.css" />
